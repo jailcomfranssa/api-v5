ff2b7e6dbe6cffcfe957bfddecb5ee0ba52f1007
jailcomfranssa <jail_son_0@hotmail.com>
2025-12-06 14:35:08 -0300

    feat: :rocket: configuração inicial Prisma ORM

---
+++ prisma/migrations/20251206173324_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'FUNCIONARIO', 'CLIENTE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "telefone" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'CLIENTE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
