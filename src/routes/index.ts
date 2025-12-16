e366b35218ac3767a41f105226bfc4406c46f3f4
jailcomfranssa <jail_son_0@hotmail.com>
2025-12-16 16:42:58 -0300

    feat: :hammer: add route cliente

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userRouter from "../model/User/user.routes";
 import authRoutes from "../model/Auth/auth.routes";
 import funcionarioRoutes from "../model/Funcionario/funcionario.routes";
+import clienteRoutes from "../model/Cliente/cliente.routes";
 
 const rootRoutes = Router();
 
 rootRoutes.use("/users", userRouter);
 rootRoutes.use("/auth", authRoutes);
 rootRoutes.use("/funcionario", funcionarioRoutes);
+rootRoutes.use("/cliente", clienteRoutes);
 
 export default rootRoutes;
